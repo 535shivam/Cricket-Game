8b0ac06b36dfca89bfddb093fb697201519a18d5
535shivam <shivam535patel@gmail.com>
2024-09-24 01:33:50 +0530

    Simple Cricket Game

--- cricket.js
+++ cricket.js
@@ -3,7 +3,27 @@ let userMove;
 let computerMove; 
 let computerChoice;
 let resultMsg ;
+let score;
 
+
+
+
+
+
+let localStr=localStorage.getItem('score');
+resetScore(localStr);
+
+//To reset the local storage and see in the alert box.
+function resetScore(localStr){
+     score=localStr ? JSON.parse(localStr):{
+        win:0,
+        lost:0,
+        tie:0,
+    };
+    score.displayScore= function(){
+        return `Win ${score.win}  Lost ${score.lost}  Tie ${score.tie}`;
+    }; 
+}
 
 
 
@@ -22,9 +42,14 @@ function compGenChoice(){
 }
 
 
-
+//To see the result in the alert Box.
 function finalResult(userMove , computerMove , result){
-    alert(`You have choosen ${userMove}. Computer Choice is ${computerMove} and ${result}.`);
+    localStorage.setItem('score' , JSON.stringify(score));
+    alert(`You have choosen ${userMove}. Computer Choice is ${computerMove} 
+    
+    ${result}.
+    
+    ${score.displayScore()}`);
 }
 
 
@@ -47,87 +72,57 @@ function showBallFuntion(){
     finalResult('Stump' , computerChoice ,resultMsg);
 }
 
+function showResetFunction(){
+    localStorage.clear();
+    resetScore();
+}
+
 
 //To check user move and computer move.
 function getResult(userMove , computerMove){
     if (userMove==='Bat'){
         if(computerMove==='Ball'){
+            score.win++;
             return 'User Won';
         }
         else if(computerMove==='Bat'){
+            score.tie++;
             return `It's Tie.`;
         }
         else if(computerMove==='Stump'){
+            score.lost++;
             return 'Computer has Won.';
         } 
     }
     else if (userMove==='Ball'){
         if(computerMove==='Ball'){
+            score.tie++;
             return `It's Tie`;
             }
             else if(computerMove==='Bat'){
+                score.lost++;
                 return 'Computer has Won';
             }
             else if(computerMove==='Stump'){
+                score.win++;
                 return 'User Won';
             }
     }
 
     else{
         if(computerMove==='Ball'){
+            score.lost++;
             return 'Computer has Won';
         }
         else if(computerMove==='Bat'){
+            score.win++;
             return 'User Won';
         }
         else if(computerMove==='Stump'){
+            score.tie++;
             return `It's Tie`;
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
